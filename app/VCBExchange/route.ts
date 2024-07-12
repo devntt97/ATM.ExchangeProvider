2b54e2c10958832bd5e715f1f76c9a411adbb361
Unmatched TaiNguyen <tainguyen.ntt.97@gmail.com>
2024-07-12 19:41:17 +0700

    update

--- app/VCBExchange/route.ts
+++ app/VCBExchange/route.ts
@@ -6,3 +6,8 @@ export const GET = async () => {
     const data = await service.GetExchangeRateVCBTableAsync()
     return new NextResponse(JSON.stringify(data))
 }
+export const PUT = async () => {
+    const service = new ExchangeRateService()
+    const data = await service.RefreshExchangeVCBTableAsync()
+    return new NextResponse(JSON.stringify(data))
+}
--- sevices/ExchangeRateService.ts
+++ sevices/ExchangeRateService.ts
@@ -46,50 +46,54 @@ export const StoreCache = new Store()
 const VCBExchangeKey = "VCBExchangeKey"
 export class ExchangeRateService {
     public static readonly UrlVCB = "https://portal.vietcombank.com.vn/UserControls/TVPortal.TyGia/pListTyGia.aspx?txttungay={0}&BacrhID=1&isEn=False";
+    public async RefreshExchangeVCBTableAsync() {
+        let exchangeRates: { [key: string]: number } = {};
+        const url = ExchangeRateService.UrlVCB.replace("{0}", new Date(Date.now() + 7 * 60 * 60 * 1000).toLocaleDateString('en-GB'));
+        let config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: url,
+            headers: {
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5,zh-CN;q=0.4,zh;q=0.3',
+                'cache-control': 'no-cache',
+                'pragma': 'no-cache',
+                'priority': 'u=0, i',
+                'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'none',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+            }
+        };
 
+        const response = await axios.request(config)
+
+        const content = response.data;
+        const $ = cheerio.load(content);
+        const rows = $('tr');
+        rows.each((_, row) => {
+            const columns = $(row).find('td');
+            if (columns.length > 0) {
+                const key = $(columns[1]).text().trim();
+                const value = parseFloat($(columns[2]).text().replace(/,/, ''));
+                if (!isNaN(value)) {
+                    exchangeRates[key] = value;
+                }
+            }
+        });
+        StoreCache.setItem(VCBExchangeKey, JSON.stringify(exchangeRates), 6 * 3600)
+        return exchangeRates
+    }
     public async GetExchangeRateVCBTableAsync(cancellationToken?: AbortSignal): Promise<{ [key: string]: number }> {
         const cacheValue = StoreCache.getItem(VCBExchangeKey)
         let exchangeRates: { [key: string]: number } = cacheValue ? JSON.parse(cacheValue) : {};
         if (!cacheValue) {
-            const url = ExchangeRateService.UrlVCB.replace("{0}", new Date(Date.now() + 7 * 60 * 60 * 1000).toLocaleDateString('en-GB'));
-            let config = {
-                method: 'get',
-                maxBodyLength: Infinity,
-                url: url,
-                headers: {
-                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                    'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5,zh-CN;q=0.4,zh;q=0.3',
-                    'cache-control': 'no-cache',
-                    'pragma': 'no-cache',
-                    'priority': 'u=0, i',
-                    'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-                    'sec-ch-ua-mobile': '?0',
-                    'sec-ch-ua-platform': '"Windows"',
-                    'sec-fetch-dest': 'document',
-                    'sec-fetch-mode': 'navigate',
-                    'sec-fetch-site': 'none',
-                    'sec-fetch-user': '?1',
-                    'upgrade-insecure-requests': '1',
-                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-                }
-            };
-
-            const response = await axios.request(config)
-
-            const content = response.data;
-            const $ = cheerio.load(content);
-            const rows = $('tr');
-            rows.each((_, row) => {
-                const columns = $(row).find('td');
-                if (columns.length > 0) {
-                    const key = $(columns[1]).text().trim();
-                    const value = parseFloat($(columns[2]).text().replace(/,/, ''));
-                    if (!isNaN(value)) {
-                        exchangeRates[key] = value;
-                    }
-                }
-            });
-            StoreCache.setItem(VCBExchangeKey, JSON.stringify(exchangeRates), 6 * 3600)
+            exchangeRates = await this.RefreshExchangeVCBTableAsync()
             console.log(exchangeRates, "no cache");
         } else {
             console.log(exchangeRates, "cache");
